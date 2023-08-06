96a728104864d1bdd6dd0df75fd798a9dc7834bd
Maarten Beeckmans <maartenb@inuits.eu>
2023-08-06 02:48:25 +0200

    Configure pacman

--- http/chroot.sh
+++ http/chroot.sh
@@ -12,4 +12,10 @@ pacman --sync --noconfirm btrfs-progs
 
 pacman --sync --noconfirm vim tree curl httpie wget htop iftop iotop tmux inetutils tar sed net-tools the_silver_searcher bind
 
+sed -i -e "s/.*ParallelDownloads.*/ParallelDownloads = 10/g" /etc/pacman.conf
+sed -i -e "s/.*Color.*/Color/g" /etc/pacman.conf
+sed -i -e "s/.*CheckSpace.*/CheckSpace/g" /etc/pacman.conf
+sed -i -e "s/.*VerbosePkgLists.*/VerbosePkgLists/g" /etc/pacman.conf
+sed -i -e "s/.*TotalDownload.*/TotalDownload/g" /etc/pacman.conf
+
 echo 'root:secret' | chpasswd
